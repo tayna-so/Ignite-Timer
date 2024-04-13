0807e18cd3e14523e726aabad1eee62ec1bbcb30
Tayna Sousa <tayna.igt@hotmail.com>
2024-04-13 15:18:58 -0300

    refactor: change useState to useReducer

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -36,9 +36,8 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate
-        );
-
+          new Date(activeCycle.startDate),
+        )
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished();
           setSecondsPassed(totalSeconds);
